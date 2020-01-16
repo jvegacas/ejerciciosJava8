4e2df5d43b8a055a35cbc9eb25af81ea15c189d8
Javier Vega <javier.vega@es.gfi.world>
2020-01-16 15:41:23 +0100

    (develop) Subida prueba

--- ejerciciosJava8/src/main/java/com/practicas/java8/EjerciciosJava8Application.java
+++ ejerciciosJava8/src/main/java/com/practicas/java8/EjerciciosJava8Application.java
@@ -3,11 +3,13 @@ package com.practicas.java8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class EjerciciosJava8Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EjerciciosJava8Application.class, args);
+		int a =5;
 	}
 
 }
